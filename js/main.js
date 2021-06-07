263ea84f44fd042be458f5b24a8480e0e08246bb
Tuiaara87 <77272499+Tuiaara87@users.noreply.github.com>
2021-06-07 11:41:38 +0300

    Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-function randomNumber(min, max) {
-    const r = Math.random() * (max - min) + min
-    return Math.floor(r)
+function findRandomNumber(min, max) {
+  const r = Math.random() * (max - min) + min
+  return Math.floor(r)
 }
 
-console.log(randomNumber);
+console.log(findRandomNumber(0, 100));
 
 // источник https://dev-gang.ru/article/javascript-generirovat-sluczainoe-czislo-v-diapazone-s1azn94f4k/
 
 
+function checkLine(line, maxLine) {
+  let lengthLine = line.length
+  if (lengthLine < maxLine) {
+    return true;
+  } else {
+    return false;
+  }
 
-let start = '31.05.2021 14:20';
-let finish = '01.05.2021 06:00';
-
-function calcMinutes(start, finish) {
-    return ((new Date(finish)).getTime() - (new Date(start)).getTime()) / 60000;
 }
-
-let minutes = calcMinutes(start, finish);
-console.log(minutes);
-
-// попыталась посчитать количество минут между двумя датами
+console.log(checkLine('hello, world', 10))
