79feccc78b17f10d069828a9b5f04f1d9ebf86cb
Tuiaara87 <77272499+Tuiaara87@users.noreply.github.com>
2021-06-22 11:49:33 +0300

    Правки по айдишникам

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
 function findRandomNumber(min, max) {
-  const r = Math.random() * (max - min) + min
-  return Math.floor(r)
-}
-
-console.log(findRandomNumber(0, 100));
-
-// источник https://dev-gang.ru/article/javascript-generirovat-sluczainoe-czislo-v-diapazone-s1azn94f4k/
-
-// function checkLine(line, maxLine) {
-//   let lengthLine = line.length
-//   if (lengthLine < maxLine) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-
-// }
-// console.log(checkLine('hello, world', 10))
-
-function checkLine(line, maxLine) {
-  let lengthLine = line.length
-  if (lengthLine < maxLine) {
-    return lengthLine < maxLine
+  if (min > max || min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
+    console.log('error')
+    return
   }
-}
-console.log(checkLine('hello, world', 10))
+  const NUMBER = Math.random() * (max - min) + min;
+  return Math.floor(NUMBER);
+};
+console.log(findRandomNumber(0, 5));
+
+function isSuitableStringLength(line, maxLine) {
+  const lengthLine = line.length;
+  if (lengthLine < maxLine) {
+    return lengthLine < maxLine;
+  }
+};
+console.log(isSuitableStringLength('hello, world', 10));
 
 // Домашнее задание: Объект
 
-
-
-const descriptions = [
+const DESCRIPTIONS = [
   'музыка исцеляет мою душу',
   'люблю порядок во всем',
   'дорогу осилит идущий',
@@ -39,17 +27,7 @@ const descriptions = [
   'просто хороший день',
 ];
 
-
-const comments = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const names = [
+const NAMES = [
   'Саша',
   'Маша',
   'Шура',
@@ -58,29 +36,44 @@ const names = [
   'Наташа',
 ];
 
-
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
 console.log(_);
 
-const createMyFirstObject = () => {
-  const randomIdIndex = _.random(0, 25);
-  const randomDescriptionIndex = _.random(0, descriptions.length - 1);
-  const randomNameIndex = _.random(0, names.length - 1);
-  const randomCommentIndex = _.random(0, comments.length - 1);
+const createMyFirstObject = (i) => {
+  const randomDescriptionIndex = _.random(0, DESCRIPTIONS.length - 1);
+  const randomNamesIndex = _.random(0, NAMES.length - 1);
+  const randomMessagesIndex = _.random(0, MESSAGES.length - 1);
   const randomLikesIndex = _.random(15, 200);
+  const randomCommentsIndex = _.random(0, 200);
+  const randomAvatarIndex = _.random(1, 6);
 
   return {
-    id: randomIdIndex,
-    avatar: 'img/avatar-' + randomIdIndex + '.svg', //конкатенация
-    description: descriptions[randomDescriptionIndex],
+    id: i,
+    url: 'photos/' + i + '.jpg',
+    avatar: 'img/avatar-' + i + '.svg',
+    description: DESCRIPTIONS[randomDescriptionIndex],
     likes: randomLikesIndex,
-    comments: comments[randomCommentIndex],
-    names: names[randomNameIndex],
+    comments: [
+      {
+        id: randomCommentsIndex,
+        avatar: 'img/avatar-' + randomAvatarIndex + '.svg',
+        message: MESSAGES[randomMessagesIndex],
+        name: NAMES[randomNamesIndex],
+      },
+    ],
   };
 };
 const photos = [];
-for (let i = 0; i < 25; i++) {
-  const photo = createMyFirstObject(); //я создала переменную фото, куда кладу результат выполнения функции createMyFirstObject. функция возвращает объект.
+for (let i = 1; i <= 25; i++) {
+  const photo = createMyFirstObject(i);
   photos.push(photo);
 };
 
